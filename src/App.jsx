a04ad50053ed7fe90daada5d7188eef387a49e61
carpicoder <webcarpi@gmail.com>
2024-04-16 21:24:28 +0200

    Changed title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Carpi Coder</h1>
+      <h1>Aplicación en Netlify</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
